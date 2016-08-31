fe0b6566e2f7c6a5cd74fc2e01e333b4574436ea
liushuwei <liushuwei@qiyi.com>
2016-08-31 18:23:25 +0800

    增加文件

---
+++ swiperefreshlayoutdemo/src/main/java/test/com/swiperefreshlayoutdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package test.com.swiperefreshlayoutdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        swipeRefreshLayout.setColorSchemeResources(R.color.bg_orange, R.color.bg_blue, R.color.bg_green, R.color.bg_red);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText((int) (Math.random() * 100) + "");
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 3000);
+            }
+        });
+    }
+
+    private void initView() {
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+}
