f580000c14c68e676b6783bd713f02869958dfb5
liushuwei <liushuwei@qiyi.com>
2016-11-14 19:23:05 +0800

    增加侧边栏部分代码

---
+++ app/src/main/java/test/com/demo/activity/NavigationDrawerActivity.java
@@ -0,0 +1,20 @@
+package test.com.demo.activity;
+
+import android.os.Bundle;
+
+import test.com.demo.R;
+
+
+public class NavigationDrawerActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
