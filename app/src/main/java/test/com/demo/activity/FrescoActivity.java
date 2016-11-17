84e24f85d2362317f1ad2d8a0f09433c940ea881
liushuwei <lsw305264677@163.com>
2016-11-18 01:00:29 +0800

    添加fresco圆形功能

--- app/src/main/java/test/com/demo/activity/FrescoActivity.java
+++ app/src/main/java/test/com/demo/activity/FrescoActivity.java
@@ -1,5 +1,6 @@
 package test.com.demo.activity;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.drawable.ScalingUtils;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.generic.RoundingParams;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
@@ -92,6 +94,56 @@ public class FrescoActivity extends BaseActivity {
         simpleDraweeView.setController(controller);*/
 
         //第三阶段
+        //初始化圆角圆形参数对象
+        RoundingParams rp = new RoundingParams();
+        //设置图像是否为圆形
+        rp.setRoundAsCircle(true);
+        //设置圆角半径
+//        rp.setCornersRadius(20);
+        //分别设置左上角、右上角、左下角、右下角的圆角半径
+//        rp.setCornersRadii(20,25,30,35);
+        //分别设置（前2个）左上角、(3、4)右上角、(5、6)左下角、(7、8)右下角的圆角半径
+//        rp.setCornersRadii(new float[]{20,25,30,35,40,45,50,55});
+        //设置边框颜色及其宽度
+        rp.setBorder(Color.RED,10);
+        //设置叠加颜色
+        rp.setOverlayColor(Color.GRAY);
+        //设置圆形圆角模式
+//        rp.setRoundingMethod(RoundingParams.RoundingMethod.BITMAP_ONLY);
+        //设置圆形圆角模式
+        rp.setRoundingMethod(RoundingParams.RoundingMethod.OVERLAY_COLOR);
+
+
+        //获取GenericDraweeHierarchy对象
+        GenericDraweeHierarchy hierarchy = GenericDraweeHierarchyBuilder.newInstance(getResources())
+                .setRoundingParams(rp)
+                //设置圆角半径
+//                .setRoundingParams(RoundingParams.fromCornersRadius(20))
+                //分别设置左上角、右上角、左下角、右下角的圆角半径
+//                .setRoundingParams(RoundingParams.fromCornersRadii(20,25,30,35))
+                //分别设置（前2个）左上角、(3、4)右上角、(5、6)左下角、(7、8)右下角的圆角半径
+//                .setRoundingParams(RoundingParams.fromCornersRadii(new float[]{20,25,30,35,40,45,50,55}))
+                //设置圆形圆角参数；RoundingParams.asCircle()是将图像设置成圆形
+//                .setRoundingParams(RoundingParams.asCircle())
+                //设置淡入淡出动画持续时间(单位：毫秒ms)
+                .setFadeDuration(5000)
+                //构建
+                .build();
+
+        //设置Hierarchy
+        simpleDraweeView.setHierarchy(hierarchy);
+
+        //构建Controller
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                //设置需要下载的图片地址
+                .setUri(imageUrl)
+                //设置点击重试是否开启
+                .setTapToRetryEnabled(true)
+                //构建
+                .build();
+
+        //设置Controller
+        simpleDraweeView.setController(controller);
 
     }
 }
