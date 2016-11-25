ff4b6fcc0144a0769cbd3da598c3aa2602af7a72
liushuwei <liushuwei@qiyi.com>
2016-11-25 21:09:47 +0800

    增加fresco的使用方式

--- app/src/main/java/test/com/demo/DemoApplication.java
+++ app/src/main/java/test/com/demo/DemoApplication.java
@@ -42,30 +42,7 @@ public class DemoApplication extends Application {
         Fresco.initialize(this);
 
         //创建默认的ImageLoader配置参数
-        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(this);
-
-        /*File cacheDir = StorageUtils.getCacheDirectory(this);  //缓存文件夹路径
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
-                .memoryCacheExtraOptions(480, 800) // default = device screen dimensions 内存缓存文件的最大长宽
-                .diskCacheExtraOptions(480, 800, null)  // 本地缓存的详细信息(缓存的最大长宽)，最好不要设置这个
-//                .taskExecutor(...).taskExecutorForCachedImages(...).threadPoolSize(3) // default  线程池内加载的数量
-                .threadPriority(Thread.NORM_PRIORITY - 2) // default 设置当前线程的优先级
-                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
-                .denyCacheImageMultipleSizesInMemory()
-                .memoryCache(new LruMemoryCache(2 * 1024 * 1024)) //可以通过自己的内存缓存实现
-                .memoryCacheSize(2 * 1024 * 1024)  // 内存缓存的最大值
-                .memoryCacheSizePercentage(13) // default
-//                .diskCache(new UnlimitedDiscCache(cacheDir)) // default 可以自定义缓存路径
-                .diskCacheSize(50 * 1024 * 1024) // 50 Mb sd卡(本地)缓存的最大值
-                .diskCacheFileCount(100)  // 可以缓存的文件数量
-                // default为使用HASHCODE对UIL进行加密命名， 还可以用MD5(new Md5FileNameGenerator())加密
-                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
-                .imageDownloader(new BaseImageDownloader(this)) // default
-                .imageDecoder(new BaseImageDecoder()) // default
-                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
-                .writeDebugLogs() // 打印debug log
-                .build(); //开始构建*/
-
+        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(getApplicationContext());
         //Initialize ImageLoader with configuration.
         ImageLoader.getInstance().init(configuration);
     }
--- app/src/main/java/test/com/demo/activity/FrescoActivity.java
+++ app/src/main/java/test/com/demo/activity/FrescoActivity.java
@@ -1,13 +1,17 @@
 package test.com.demo.activity;
 
 import android.graphics.Color;
+import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.renderscript.RenderScript;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.drawable.ScalingUtils;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
@@ -60,7 +64,7 @@ public class FrescoActivity extends BaseActivity {
 
         //第二阶段
         //初始化多张背景图集合
-       /* List<Drawable> bgs = new ArrayList<Drawable>();
+        List<Drawable> bgs = new ArrayList<Drawable>();
         bgs.add(ContextCompat.getDrawable(this,R.drawable.bg_zero));
         bgs.add(ContextCompat.getDrawable(this,R.drawable.bg_one));
         bgs.add(ContextCompat.getDrawable(this,R.drawable.bg_two));
@@ -71,31 +75,71 @@ public class FrescoActivity extends BaseActivity {
         overlays.add(ContextCompat.getDrawable(this,R.drawable.overlay_two));
         overlays.add(ContextCompat.getDrawable(this,R.drawable.overlay_three));
 
+        RoundingParams rp = new RoundingParams();
+        //设置图像是否为圆形
+        rp.setRoundAsCircle(true);
+        //设置圆角半径
+//        rp.setCornersRadius(20);
+        //分别设置左上角、右上角、左下角、右下角的圆角半径
+//        rp.setCornersRadii(20,25,30,35);
+        //分别设置（前2个）左上角、(3、4)右上角、(5、6)左下角、(7、8)右下角的圆角半径
+//        rp.setCornersRadii(new float[]{20,25,30,35,40,45,50,55});
+        //设置边框颜色及其宽度
+        rp.setBorder(Color.RED,10);
+        //设置叠加颜色
+        rp.setOverlayColor(Color.GRAY);
+        //设置圆形圆角模式
+//        rp.setRoundingMethod(RoundingParams.RoundingMethod.BITMAP_ONLY);
+        //设置圆形圆角模式
+        rp.setRoundingMethod(RoundingParams.RoundingMethod.OVERLAY_COLOR);
+
+
         GenericDraweeHierarchy genericDraweeHierarchy = new GenericDraweeHierarchyBuilder(getResources())
                 .setFadeDuration(5000)
-                .setBackground(ContextCompat.getDrawable(this, R.drawable.bg_zero))
-                .setBackgrounds(bgs)
-                .setOverlay(ContextCompat.getDrawable(this, R.drawable.overlay_one))
+//                .setBackground(ContextCompat.getDrawable(this, R.drawable.bg_zero))
+//                .setBackgrounds(bgs)
+//                .setOverlay(ContextCompat.getDrawable(this, R.drawable.overlay_one))
 //                .setOverlays(overlays)
                 .setPlaceholderImage(ContextCompat.getDrawable(this, R.drawable.icon_placeholder), ScalingUtils.ScaleType.CENTER_CROP)
                 .setProgressBarImage(ContextCompat.getDrawable(this,R.drawable.icon_progress_bar),ScalingUtils.ScaleType.CENTER_CROP)
                 .setFailureImage(ContextCompat.getDrawable(this,R.drawable.icon_failure),ScalingUtils.ScaleType.CENTER_CROP)
                 .setRetryImage(ContextCompat.getDrawable(this,R.drawable.icon_retry),ScalingUtils.ScaleType.CENTER_CROP)
+//                .setRoundingParams(rp)
+                //设置圆角半径
+                .setRoundingParams(RoundingParams.fromCornersRadius(20))
+                //分别设置左上角、右上角、左下角、右下角的圆角半径
+                .setRoundingParams(RoundingParams.fromCornersRadii(20,25,30,35))
+                //设置圆形圆角参数；RoundingParams.asCircle()是将图像设置成圆形
+                .setRoundingParams(RoundingParams.asCircle())
                 .build();
         simpleDraweeView.setHierarchy(genericDraweeHierarchy);
-        simpleDraweeView.setImageURI(imageUrl);
+
+        ControllerListener controllerListener = new BaseControllerListener(){
+
+            @Override
+            public void onFinalImageSet(String id, Object imageInfo, Animatable animatable) {
+                super.onFinalImageSet(id, imageInfo, animatable);
+                Log.i("lsw","onFinalImageSet");
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                super.onFailure(id, throwable);
+                Log.i("lsw","onFailure");
+            }
+        };
 
         DraweeController controller = Fresco.newDraweeControllerBuilder()
                 .setUri(imageUrl)
                 .setTapToRetryEnabled(true)
-                .setOldController(simpleDraweeView.getController())
+                .setControllerListener(controllerListener)
                 .build();
-        //在setHierarchy和setImageURI两个方法之后
-        simpleDraweeView.setController(controller);*/
+
+        simpleDraweeView.setController(controller);
 
         //第三阶段
         //初始化圆角圆形参数对象
-        RoundingParams rp = new RoundingParams();
+        /*RoundingParams rp = new RoundingParams();
         //设置图像是否为圆形
         rp.setRoundAsCircle(true);
         //设置圆角半径
@@ -143,7 +187,56 @@ public class FrescoActivity extends BaseActivity {
                 .build();
 
         //设置Controller
-        simpleDraweeView.setController(controller);
+        simpleDraweeView.setController(controller);*/
+
+        //第四阶段
+        /*ControllerListener controllerListener = new BaseControllerListener(){
+
+            @Override
+            public void onSubmit(String id, Object callerContext) {
+                super.onSubmit(id, callerContext);
+                Log.i("lsw","onSubmit");
+            }
+
+            @Override
+            public void onFinalImageSet(String id, Object imageInfo, Animatable animatable) {
+                super.onFinalImageSet(id, imageInfo, animatable);
+                Log.i("lsw","onFinalImageSet");
+            }
+
+            @Override
+            public void onIntermediateImageSet(String id, Object imageInfo) {
+                super.onIntermediateImageSet(id, imageInfo);
+                Log.i("lsw","onIntermediateImageSet");
+            }
+
+            @Override
+            public void onIntermediateImageFailed(String id, Throwable throwable) {
+                super.onIntermediateImageFailed(id, throwable);
+                Log.i("lsw","onIntermediateImageFailed");
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                super.onFailure(id, throwable);
+                Log.i("lsw","onFailure");
+            }
+
+            @Override
+            public void onRelease(String id) {
+                super.onRelease(id);
+                Log.i("lsw","onRelease");
+            }
+        };
+
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                //加载的图片URI地址
+                .setUri(imageUrl)
+                .setControllerListener(controllerListener)
+                .build();
+
+        //设置DraweeController
+        simpleDraweeView.setController(controller);*/
 
     }
 }
