528cafe58fbc19fc28b5101a2534541f1004a645
liushuwei <liushuwei@qiyi.com>
2017-02-24 19:19:23 +0800

    增加Xml解析

--- app/src/main/java/test/com/demo/activity/RxJavaActivity.java
+++ app/src/main/java/test/com/demo/activity/RxJavaActivity.java
@@ -8,6 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -20,6 +26,14 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -190,10 +204,149 @@ public class RxJavaActivity extends AppCompatActivity {
                 getSystemAllFallBackFonts();
                 break;
             case R.id.button3:
-//                getSystemEnglishFonts();
+                getSystemEnglishFonts();
                 break;
             case R.id.button4:
+//                test();
+                test1();
                 break;
+        }
+    }
+
+    private void test1() {
+        String path = Environment.getRootDirectory().getPath() + File.separator + "etc" + File.separator + "fonts.xml";
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setValidating(false);
+        DocumentBuilder db = null;
+        try {
+            db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        Document doc = null;
+        try {
+            doc = db.parse(new FileInputStream(new File(path)));
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        XPathFactory factory = XPathFactory.newInstance();
+
+        XPath xpath = factory.newXPath();
+
+        String expression;
+        NodeList nodeList = null;
+
+        expression = "familyset/family[@lang=\"zh-Hans\"]/font";
+
+        try {
+            nodeList = (NodeList) xpath.evaluate(expression, doc, XPathConstants.NODESET);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+
+        if (nodeList == null) {
+
+        }
+        for (int i = 0; i < nodeList.getLength(); i++) {
+
+            System.out.print(nodeList.item(i).getChildNodes().item(0).getTextContent() + " ");
+
+        }
+    }
+    private void test() {
+        String path = Environment.getRootDirectory().getPath() + File.separator + "etc" + File.separator + "fonts.xml";
+        File file = new File(path);
+        readXML(file);
+    }
+
+    private void readXML(File file) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+            Element element = doc.getDocumentElement();
+            listAllChildNodes(element, 0);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * 递归遍历并打印所有的ElementNode(包括节点的属性和文本节点的有效内容),按一般的xml样式展示出来(空格来表示层次)
+     */
+    public void listAllChildNodes(Node node, int level) {
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+            if("family".equals(node.getNodeName())){
+                if(node.hasAttributes()){
+                    NamedNodeMap nnmap = node.getAttributes();
+                    for (int i = 0; i < nnmap.getLength(); i++) {
+                        if("lang".equals(nnmap.item(i).getNodeName())){
+                            if("zh-Hans".equals(nnmap.item(i).getNodeValue())){
+                                if(node.hasChildNodes()){
+                                    level++;
+                                    NodeList nodelist = node.getChildNodes();
+                                    for (int j = 0; j < nodelist.getLength(); j++) {
+                                        if (nodelist.item(j).getNodeType() == Node.TEXT_NODE
+                                                && (!nodelist.item(i).getTextContent()
+                                                .matches("\\s+"))) {// 用正则选取内容包含非空格的有效字符的文本节点
+//                                            hasTextChild = true;// 该ElementNode的一级子节点是存在有效字符的文本节点
+                                            System.out.print(nodelist.item(i).getTextContent());// 在开始标签后面添加文本内容
+                                        } else if (nodelist.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                                            System.out.println();
+                                            listAllChildNodes(nodelist.item(i), level);// level表示该节点处于第几个层次(相应空格)
+                                        }
+                                    }
+                                    level--;// 遍历完所有的子节点,层次变量随子节点的层数,依次递减,回归到该节点本身的层次
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            boolean hasTextChild = false;
+            String levelSpace = "";
+            for (int i = 0; i < level; i++) {
+                levelSpace += "    ";
+            }
+            Log.i(TAG, "listAllChildNodes: "+(levelSpace + "<" + node.getNodeName() + (node.hasAttributes() ? " " : ">")));// 有属性的话节点的开始标签后面的尖括号">"就留待属性打印完再打印
+            if (node.hasAttributes()) {
+                NamedNodeMap nnmap = node.getAttributes();
+                for (int i = 0; i < nnmap.getLength(); i++) {
+                    System.out.print(nnmap.item(i).getNodeName()
+                            + "=\""// 字符串里含双引号要用到转义字符\
+                            + nnmap.item(i).getNodeValue() + "\""
+                            + (i == (nnmap.getLength() - 1) ? "" : " "));// 不是最后一个属性的话属性之间要留空隙
+                }
+                System.out.print(">");// 开始标签里的属性全部打印完加上尖括号">"
+            }
+            if (node.hasChildNodes()) {
+                level++;// 有下一级子节点,层次加1,新的层次表示的是这个子节点的层次(递归调用时传给了它)
+                NodeList nodelist = node.getChildNodes();
+                for (int i = 0; i < nodelist.getLength(); i++) {
+                    if (nodelist.item(i).getNodeType() == Node.TEXT_NODE
+                            && (!nodelist.item(i).getTextContent()
+                            .matches("\\s+"))) {// 用正则选取内容包含非空格的有效字符的文本节点
+                        hasTextChild = true;// 该ElementNode的一级子节点是存在有效字符的文本节点
+                        System.out.print(nodelist.item(i).getTextContent());// 在开始标签后面添加文本内容
+                    } else if (nodelist.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                        System.out.println();
+                        listAllChildNodes(nodelist.item(i), level);// level表示该节点处于第几个层次(相应空格)
+                    }
+                }
+                level--;// 遍历完所有的子节点,层次变量随子节点的层数,依次递减,回归到该节点本身的层次
+            }
+            System.out.print(((hasTextChild) ? "" : "\n" + levelSpace) + "</"
+                    + node.getNodeName() + ">");
         }
     }
 
@@ -202,15 +355,14 @@ public class RxJavaActivity extends AppCompatActivity {
      * 遍历获取系统所有字体
      * @return
      */
-/*    public static void getSystemEnglishFonts() {
+    public static void getSystemEnglishFonts() {
 
-        String path = Environment.getRootDirectory().getPath() + File.separator + "etc" + File.separator + "system_fonts.xml";
-        String path = Environment.getRootDirectory().getPath() + File.separator + "etc" + File.separator + "system_fonts.xml";
-        List<String> fontsList= parseXMLWithPull(getHtmlTemplate(path,"system_fonts.xml"));
+        String path = Environment.getRootDirectory().getPath() + File.separator + "etc" + File.separator + "fonts.xml";
+        List<String> fontsList= parseFontsXMLWithPull(getFontXMLStr(path));
         for (int i = 0; i < fontsList.size(); i++) {
             Log.d(TAG, "getSystemEnglishFonts: "+fontsList.get(i));
         }
-    }*/
+    }
 
 
     /**
@@ -270,6 +422,123 @@ public class RxJavaActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return list;
+    }
+
+    private static List<String> parseFontsXMLWithPull(String xmlData) {
+        List<String> list = null;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //文档开始
+                    case XmlPullParser.START_DOCUMENT:
+                        list = new ArrayList<>();
+                        break;
+                    // 开始解析某个结点
+                    case XmlPullParser.START_TAG: {
+
+                        if("family".equals(nodeName)){
+                            boolean isRightChineseFont = false;
+                            int count = xmlPullParser.getAttributeCount();
+                            for (int i = 0; i < count; i++) {
+                                String name = xmlPullParser.getAttributeName(i);
+                                String value = xmlPullParser.getAttributeValue(i);
+                                if (name.equals("lang")) {
+                                    if("zh-Hans".equals(value)){
+                                        isRightChineseFont = true;
+                                        list.clear();
+                                        list.add(xmlPullParser.nextText());
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if(isRightChineseFont){
+                                break;
+                            }
+
+                        }else if("font".equals(nodeName)){
+                            boolean isRightWeight = false;
+                            boolean isRightStyle = false;
+                            int count = xmlPullParser.getAttributeCount();
+                            for (int i = 0; i < count; i++) {
+                                String name = xmlPullParser.getAttributeName(i);
+                                String value = xmlPullParser.getAttributeValue(i);
+                                if("weight".equals(name)){
+                                    if("400".equals(value)){
+                                        isRightStyle = true;
+                                    }
+                                }else if("style".equals(name)){
+                                    if("normal".equals(value)){
+                                        isRightStyle = true;
+                                    }
+                                }
+                            }
+                            if((isRightWeight)&&(isRightStyle)){
+                                list.add(xmlPullParser.nextText());
+                            }
+                        }
+                        break;
+                    }
+                    // 完成解析某个结点
+                    case XmlPullParser.END_TAG: {
+
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private static String parseEnglishFontsXMLWithPull(String xmlData) {
+        String englishFont = "";
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //文档开始
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    // 开始解析某个结点
+                    case XmlPullParser.START_TAG: {
+                        if("font".equals(nodeName)){
+                            if("400".equals(xmlPullParser.getAttributeValue(0))){
+                                if("normal".equals(xmlPullParser.getAttributeValue(1))){
+                                    englishFont = xmlPullParser.nextText();
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    // 完成解析某个结点
+                    case XmlPullParser.END_TAG: {
+
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return englishFont;
     }
 
     /**
