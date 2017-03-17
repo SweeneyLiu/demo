c15e2e6f08e3a5e5460838101c07c09c4802a7e1
liushuwei <liushuwei@qiyi.com>
2017-03-17 15:23:26 +0800

    调整主函数的实现方式

--- app/src/main/java/test/com/demo/activity/MainActivity.java
+++ app/src/main/java/test/com/demo/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package test.com.demo.activity;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,9 +14,83 @@ import butterknife.OnClick;
 import test.com.demo.R;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends ListActivity {
 
-    @BindView(R.id.recyclerview_button)
+
+    public static final String[] testOptions = { "RecyclerView测试", "SwipeRefreshLayout测试", "ViewPager测试", "ViewFlipper测试", "Volley测试",
+            "DrawerLayout测试", "fresco测试", "ImageLoader测试", "picasso测试", "glide测试", "RxJava测试", "Permission测试", "百度地图测试",
+            "MaterialDesign测试", "下载测试", "Notification测试", "动态增加View测试"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, testOptions));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent;
+
+        switch (position) {
+            default:
+            case 0:
+                intent = new Intent(this, RecyclerViewActivity.class);
+                break;
+            case 1:
+                intent = new Intent(this, SwipeRefreshLayoutActivity.class);
+                break;
+            case 2:
+                intent = new Intent(this, ViewPagerActivity.class);
+                break;
+            case 3:
+                intent = new Intent(this, ViewFlipperActivity.class);
+                break;
+            case 4:
+                intent = new Intent(this, VolleyActivity.class);
+                break;
+            case 5:
+                intent = new Intent(this, NavigationDrawerActivity.class);
+                break;
+            case 6:
+                intent = new Intent(this, FrescoActivity.class);
+                break;
+            case 7:
+                intent = new Intent(this, ImageLoaderActivity.class);
+                break;
+            case 8:
+                intent = new Intent(this, PicassoActivity.class);
+                break;
+            case 9:
+                intent = new Intent(this, GlideActivity.class);
+                break;
+            case 10:
+                intent = new Intent(this, RxJavaActivity.class);
+                break;
+            case 11:
+                intent = new Intent(this, PermissionActivity.class);
+                break;
+            case 12:
+                intent = new Intent(this, BaiDuMapActivity.class);
+                break;
+            case 13:
+                intent = new Intent(this, MaterialDesignActivity.class);
+                break;
+            case 14:
+                intent = new Intent(this, DownloadActivity.class);
+                break;
+            case 15:
+                intent = new Intent(this, NotificationActivity.class);
+                break;
+            case 16:
+                intent = new Intent(this, DynamicAddViewActivity.class);
+                break;
+        }
+
+        startActivity(intent);
+    }
+
+    /*@BindView(R.id.recyclerview_button)
     Button recyclerviewButton;
     @BindView(R.id.swiperefreshlayout_button)
     Button swiperefreshlayoutButton;
@@ -139,5 +216,5 @@ public class MainActivity extends BaseActivity {
                 startActivity(intent);
                 break;
         }
-    }
+    }*/
 }
